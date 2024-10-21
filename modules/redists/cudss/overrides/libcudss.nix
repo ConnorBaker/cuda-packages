79369a33e4269eb5545a3ad85ea391684d552ab6
Connor Baker <ConnorBaker01@gmail.com>
2024-10-21 18:24:59 +0000

    libcudss: override should patch CMake files

--- modules/redists/cudss/overrides/libcudss.nix
+++ modules/redists/cudss/overrides/libcudss.nix
@@ -5,6 +5,7 @@
   nccl,
 }:
 let
+  inherit (builtins) placeholder;
   inherit (lib.lists) optionals;
 in
 prevAttrs: {
@@ -15,4 +16,32 @@ prevAttrs: {
       mpi
       nccl
     ];
+
+  # Update the CMake configurations
+  postFixup =
+    prevAttrs.postFixup or ""
+    + optionals (cudaAtLeast "12.6") (
+      # Enter the directory containing the CMake configurations
+      ''
+        pushd "$dev/lib/cmake/cudss"
+      ''
+      # Patch cudss-config.cmake to fix the relative paths so they refer to our splayed outputs.
+      + ''
+        substituteInPlace cudss-config.cmake \
+          --replace-fail \
+           'get_filename_component(_cudss_search_prefix "''${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)' \
+           'set(_cudss_search_prefix "${placeholder "dev"}/lib;${placeholder "lib"}/lib;${placeholder "include"}/include")'
+      ''
+      # Patch cudss-targets-release.cmake to fix the path to the static library.
+      + ''
+        substituteInPlace cudss-targets-release.cmake \
+        --replace-fail \
+         '"''${cudss_LIBRARY_DIR}/libcudss_static.a"' \
+         '"${placeholder "static"}/lib/libcudss_static.a"'
+      ''
+      # Return to the original directory
+      + ''
+        popd
+      ''
+    );
 }
