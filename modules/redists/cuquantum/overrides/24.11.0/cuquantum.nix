ee9e0150047a6c9077dfa5e1352e1b31cf6d6338
Connor Baker <ConnorBaker01@gmail.com>
2024-11-22 21:31:59 -0800

    cuquantum: add dependency on cuda_cudart

--- modules/redists/cuquantum/overrides/24.11.0/cuquantum.nix
+++ modules/redists/cuquantum/overrides/24.11.0/cuquantum.nix
@@ -1,10 +1,12 @@
 {
+  cuda_cudart,
   libcublas,
   libcusolver,
   libcutensor,
 }:
 prevAttrs: {
   buildInputs = prevAttrs.buildInputs or [ ] ++ [
+    cuda_cudart
     libcublas
     libcusolver
     libcutensor
