26fcca8cfac7a5c5eaef71df9a047ce0b2086cf2
Connor Baker <ConnorBaker01@gmail.com>
2024-12-29 18:36:52 +0000

    libcudss: fixup override for 0.4 release

--- modules/redists/cudss/overrides/common/libcudss.nix
+++ modules/redists/cudss/overrides/common/libcudss.nix
@@ -6,7 +6,7 @@
 }:
 let
   inherit (builtins) placeholder;
-  inherit (lib.lists) optionals;
+  inherit (lib.lists) elem optionals;
   inherit (lib.strings) optionalString versionAtLeast versionOlder;
 in
 finalAttrs: prevAttrs: {
@@ -23,30 +23,34 @@ finalAttrs: prevAttrs: {
   # Update the CMake configurations
   postFixup =
     prevAttrs.postFixup or ""
-    + optionalString (versionOlder finalAttrs.version "0.4") (
-      # Enter the directory containing the CMake configurations
-      ''
-        pushd "$dev/lib/cmake/cudss"
-      ''
-      # Patch cudss-config.cmake to fix the relative paths so they refer to our splayed outputs.
-      + ''
-        nixLog "patching cudss-config.cmake to fix relative paths"
-        substituteInPlace cudss-config.cmake \
-          --replace-fail \
-           'get_filename_component(_cudss_search_prefix "''${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)' \
-           'set(_cudss_search_prefix "${placeholder "dev"}/lib;${placeholder "lib"}/lib;${placeholder "include"}/include")'
-      ''
-      # Patch cudss-targets-release.cmake to fix the path to the static library.
-      + ''
-        nixLog "patching cudss-targets-release.cmake to fix the path to the static library"
-        substituteInPlace cudss-targets-release.cmake \
-          --replace-fail \
-          '"''${cudss_LIBRARY_DIR}/libcudss_static.a"' \
-          '"${placeholder "static"}/lib/libcudss_static.a"'
-      ''
-      # Return to the original directory
-      + ''
-        popd
-      ''
-    );
+    # Enter the directory containing the CMake configurations
+    + ''
+      pushd "$dev/lib/cmake/cudss"
+    ''
+    + optionalString (versionOlder finalAttrs.version "0.4") ''
+      nixLog "patching cudss-config.cmake pre-0.4 release to fix relative paths"
+      substituteInPlace cudss-config.cmake \
+        --replace-fail \
+         'get_filename_component(_cudss_search_prefix "''${CMAKE_CURRENT_LIST_DIR}/../../" ABSOLUTE)' \
+         'set(_cudss_search_prefix "${placeholder "dev"}/lib;${placeholder "lib"}/lib;${placeholder "include"}/include")'
+    ''
+    + optionalString (versionAtLeast finalAttrs.version "0.4") ''
+      nixLog "patching cudss-config.cmake post-0.4 release to fix relative paths"
+      substituteInPlace cudss-config.cmake \
+        --replace-fail \
+         'get_filename_component(PACKAGE_PREFIX_DIR "''${CMAKE_CURRENT_LIST_DIR}/../../../../" ABSOLUTE)' \
+         'set(_cudss_search_prefix "${placeholder "dev"}/lib;${placeholder "lib"}/lib;${placeholder "include"}/include")'
+    ''
+    # NOTE: Only the pre-0.4 release includes the line about the static library.
+    + optionalString (versionOlder finalAttrs.version "0.4") ''
+      nixLog "patching cudss-targets-release.cmake pre-0.4 release to fix the path to the static library"
+      substituteInPlace cudss-targets-release.cmake \
+        --replace-fail \
+        '"''${cudss_LIBRARY_DIR}/libcudss_static.a"' \
+        '"${placeholder "static"}/lib/libcudss_static.a"'
+    ''
+    # Return to the original directory
+    + ''
+      popd
+    '';
 }
