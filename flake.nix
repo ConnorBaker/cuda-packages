fa864edbdad0103163b82b6c0b7e4f90e5eb5969
Connor Baker <ConnorBaker01@gmail.com>
2025-01-23 09:06:41 +0000

    flake: bump and test against only master (goal is to merge before 25.05 cutoff)

--- flake.nix
+++ flake.nix
@@ -5,7 +5,6 @@
       url = "github:hercules-ci/flake-parts";
     };
     nixpkgs.url = "github:NixOS/nixpkgs";
-    nixpkgs-24_11.url = "github:NixOS/nixpkgs/release-24.11";
     git-hooks-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:cachix/git-hooks.nix";
@@ -27,14 +26,9 @@
         "aarch64-linux"
         "x86_64-linux"
       ];
-      # NOTE: Must match the names in inputs
-      nixpkgsVersions = [
-        "nixpkgs"
-        "nixpkgs-24_11"
-      ];
       mkNixpkgs =
-        nixpkgsVersion: system:
-        import inputs.${nixpkgsVersion} {
+        system:
+        import inputs.nixpkgs {
           inherit system;
           # TODO: Due to the way Nixpkgs is built in stages, the config attribute set is not re-evaluated.
           # This is problematic for us because we use it to signal the CUDA capabilities to the overlay.
@@ -48,9 +42,7 @@
           overlays = [ inputs.self.overlays.default ];
         };
       # Memoization through lambda lifting.
-      nixpkgsInstances = genAttrs systems (
-        system: genAttrs nixpkgsVersions (nixpkgsVersion: mkNixpkgs nixpkgsVersion system)
-      );
+      nixpkgsInstances = genAttrs systems mkNixpkgs;
     in
     mkFlake { inherit inputs; } {
       inherit systems;
@@ -69,42 +61,33 @@
           # 8.9 supported by all versions of CUDA 12
           sm_89 = genAttrs systems (
             system:
-            genAttrs nixpkgsVersions (
-              nixpkgsVersion:
-              mkHydraJobsRecurseByDefault {
-                inherit (nixpkgsInstances.${system}.${nixpkgsVersion}.pkgsCuda.sm_89)
-                  cudaPackages_12_2_2
-                  cudaPackages_12_6_3
-                  ;
-              }
-            )
+            mkHydraJobsRecurseByDefault {
+              inherit (nixpkgsInstances.${system}.pkgsCuda.sm_89)
+                cudaPackages_12_2_2
+                cudaPackages_12_6_3
+                ;
+            }
           );
           # Xavier (7.2) is only supported up to CUDA 12.2.2 by cuda-compat on JetPack 5.
           # Unfortunately, NVIDIA isn't releasing support for Xavier on JetPack 6, so we're stuck.
           sm_72 = genAttrs [ "aarch64-linux" ] (
             system:
-            genAttrs nixpkgsVersions (
-              nixpkgsVersion:
-              mkHydraJobsRecurseByDefault {
-                inherit (nixpkgsInstances.${system}.${nixpkgsVersion}.pkgsCuda.sm_72)
-                  cudaPackages_12_2_2
-                  ;
-              }
-            )
+            mkHydraJobsRecurseByDefault {
+              inherit (nixpkgsInstances.${system}.pkgsCuda.sm_72)
+                cudaPackages_12_2_2
+                ;
+            }
           );
           # Orin (8.7) is only supported up to CUDA 12.2.2 by cuda-compat on JetPack 5.
           # Orin has a JetPack 6 release which allows it to run later versions of CUDA, but it has not yet been
           # packaged by https://github.com/anduril/jetpack-nixos.
           sm_87 = genAttrs [ "aarch64-linux" ] (
             system:
-            genAttrs nixpkgsVersions (
-              nixpkgsVersion:
-              mkHydraJobsRecurseByDefault {
-                inherit (nixpkgsInstances.${system}.${nixpkgsVersion}.pkgsCuda.sm_87)
-                  cudaPackages_12_2_2
-                  ;
-              }
-            )
+            mkHydraJobsRecurseByDefault {
+              inherit (nixpkgsInstances.${system}.pkgsCuda.sm_87)
+                cudaPackages_12_2_2
+                ;
+            }
           );
         };
       };
@@ -117,7 +100,7 @@
           ...
         }:
         {
-          _module.args.pkgs = nixpkgsInstances.${system}.nixpkgs;
+          _module.args.pkgs = nixpkgsInstances.${system};
 
           devShells = {
             inherit (config.packages) cuda-redist;
