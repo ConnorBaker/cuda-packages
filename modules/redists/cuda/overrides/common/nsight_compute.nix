23cd09b542d9c7965967bc50d8cea39e212510e8
Connor Baker <ConnorBaker01@gmail.com>
2025-01-23 10:05:04 +0000

    nsight: fully vendor old libtiff derivation

--- modules/redists/cuda/overrides/common/nsight_compute.nix
+++ modules/redists/cuda/overrides/common/nsight_compute.nix
@@ -1,24 +1,31 @@
 {
+  autoreconfHook,
   cuda_cudart,
   cudaConfig,
   cudaPackages,
   dbus,
   e2fsprogs,
+  fetchpatch,
+  fetchzip,
   fontconfig,
   gst_all_1,
   kdePackages,
   lib,
+  libdeflate,
+  libjpeg,
   libssh,
   libxkbcommon,
-  nsight_systems,
   nspr,
   nss,
+  pkg-config,
   qt6Packages,
   rdma-core,
   stdenv,
   ucx,
   xcb-util-cursor,
   xorg,
+  xz,
+  zlib,
 }:
 let
   inherit (cudaConfig) hostRedistArch;
@@ -47,6 +54,128 @@ let
     xcbutilrenderutil
     xcbutilwm
     ;
+
+  # Most of this is taken directly from
+  # https://github.com/NixOS/nixpkgs/blob/ea4c80b39be4c09702b0cb3b42eab59e2ba4f24b/pkgs/development/libraries/libtiff/default.nix
+  libtiff_4_4 = stdenv.mkDerivation (finalAttrs: {
+    pname = "libtiff";
+    version = "4.4.0";
+
+    src = fetchzip {
+      url = "https://download.osgeo.org/libtiff/tiff-${finalAttrs.version}.tar.gz";
+      hash = "sha256-NiqxTgfXIvUsVMu9nTJULVulfcFm+Z2IBMc6mNgwnsY=";
+    };
+
+    patches = [
+      # FreeImage needs this patch
+      (fetchpatch {
+        name = "headers.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/headers.patch";
+        hash = "sha256-+eaPCdWUyGC6rfEd54/8PqqGZ4hg8GpH75/NZgTKTt4=";
+      })
+      # libc++abi 11 has an `#include <version>`, this picks up files name
+      # `version` in the project's include paths
+      (fetchpatch {
+        name = "rename-version.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/rename-version.patch";
+        hash = "sha256-ykefUIyTqcAWX9b/CtqPsd82AsUFZZGhiL+9UmEcvU8=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-34526.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/275735d0354e39c0ac1dc3c0db2120d6f31d1990.patch";
+        hash = "sha256-faKsdJjvQwNdkAKjYm4vubvZvnULt9zz4l53zBFr67s=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-2953.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/48d6ece8389b01129e7d357f0985c8f938ce3da3.patch";
+        hash = "sha256-h9hulV+dnsUt/2Rsk4C1AKdULkvweM2ypIJXYQ3BqQU=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-3626.CVE-2022-3627.CVE-2022-3597.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/236b7191f04c60d09ee836ae13b50f812c841047.patch";
+        excludes = [ "doc/tools/tiffcrop.rst" ];
+        hash = "sha256-L2EMmmfMM4oEYeLapO93wvNS+HlO0yXsKxijXH+Wuas=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-3598.CVE-2022-3570.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/cfbb883bf6ea7bedcb04177cc4e52d304522fdff.patch";
+        hash = "sha256-SLq2+JaDEUOPZ5mY4GPB6uwhQOG5cD4qyL5o9i8CVVs=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-3970.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/227500897dfb07fb7d27f7aa570050e62617e3be.patch";
+        hash = "sha256-pgItgS+UhMjoSjkDJH5y7iGFZ+yxWKqlL7BdT2mFcH0=";
+      })
+      (fetchpatch {
+        name = "4.4.0-CVE-2022-48281.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/4.4.0-CVE-2022-48281.patch";
+        hash = "sha256-i/Gw8MvqlOwy2CupJrX7Ec1yS9xg+19CM9kKlxvvvYE=";
+      })
+      (fetchpatch {
+        name = "4.4.0-CVE-2023-0800.CVE-2023-0801.CVE-2023-0802.CVE-2023-0803.CVE-2023-0804.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/4.4.0-CVE-2023-0800.CVE-2023-0801.CVE-2023-0802.CVE-2023-0803.CVE-2023-0804.patch";
+        hash = "sha256-yKMI8AJALEV/qjJk5MhIOj+x2nHMzDvnrroWRjTcqP4=";
+      })
+      (fetchpatch {
+        name = "4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.prerequisite-0.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.prerequisite-0.patch";
+        hash = "sha256-+MHoxuzJj7ADjKz4zl4YO+e1IdzphXa451h+sRZ7gys=";
+      })
+      (fetchpatch {
+        name = "4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.prerequisite-1.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.prerequisite-1.patch";
+        hash = "sha256-+UHvo03zlmwxu1s5oRCK9SudgjcjV/iaMKW4SY/wy4E=";
+      })
+      (fetchpatch {
+        name = "4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.patch";
+        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/release-22.11/pkgs/development/libraries/libtiff/4.4.0-CVE-2023-0795.CVE-2023-0796.CVE-2023-0797.CVE-2023-0798.CVE-2023-0799.patch";
+        hash = "sha256-tMZ/ci2am9XNmD889XdjNnBUKNiN3/toMP1+QWQx+wE=";
+      })
+      (fetchpatch {
+        name = "CVE-2022-4645.patch";
+        url = "https://gitlab.com/libtiff/libtiff/-/commit/f00484b9519df933723deb38fff943dc291a793d.patch";
+        sha256 = "sha256-sFVi5BY/L8WisrtTThkux1Gw2x0UrurnSlv4KkEvw3w=";
+      })
+    ];
+
+    postPatch = ''
+      mv VERSION VERSION.txt
+    '';
+
+    outputs = [
+      "bin"
+      "dev"
+      "dev_private"
+      "out"
+      "man"
+      "doc"
+    ];
+
+    postFixup = ''
+      moveToOutput include/tif_dir.h $dev_private
+      moveToOutput include/tif_config.h $dev_private
+      moveToOutput include/tiffiop.h $dev_private
+    '';
+
+    # If you want to change to a different build system, please make
+    # sure cross-compilation works first!
+    nativeBuildInputs = [
+      autoreconfHook
+      pkg-config
+    ];
+
+    buildInputs = [ libdeflate ];
+
+    propagatedBuildInputs = [
+      libjpeg
+      xz
+      zlib
+    ];
+
+    enableParallelBuilding = true;
+
+    doCheck = true;
+  });
 in
 finalAttrs: prevAttrs: {
   allowFHSReferences = true;
@@ -146,7 +275,7 @@ finalAttrs: prevAttrs: {
     ++ optionals (finalAttrs.version == "2023.2.2.3") [
       gst-plugins-base
       gstreamer
-      nsight_systems.passthru.libtiff_4_5 # libtiff.so.5
+      libtiff_4_4 # libtiff.so.5
     ];
 
   postInstall =
