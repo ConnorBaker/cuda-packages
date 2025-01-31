a78514b79ee8397130382ac9c8e4fdb4ffbaf35c
Connor Baker <ConnorBaker01@gmail.com>
2025-01-31 06:38:35 +0000

    onnx-tensorrt,tensorrt-python: add missing includes now that they're no longer propagated by cudart

--- cuda-packages/common/onnx-tensorrt.nix
+++ cuda-packages/common/onnx-tensorrt.nix
@@ -11,7 +11,7 @@
 }:
 let
   inherit (lib.asserts) assertMsg;
-  inherit (lib.attrsets) getLib;
+  inherit (lib.attrsets) getLib getOutput;
   inherit (lib.strings) cmakeBool cmakeFeature;
   inherit (lib.versions) majorMinor;
   inherit (onnx.passthru) cppProtobuf;
@@ -107,6 +107,7 @@ let
     ];
 
     buildInputs = [
+      (getOutput "include" cuda_nvcc) # for crt/host_defines.h
       cppProtobuf
       cuda_cudart
       tensorrt
