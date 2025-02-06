dd0266aece12e5177e3ce32d62b6665c33847837
Connor Baker <ConnorBaker01@gmail.com>
2025-02-06 09:05:14 +0000

    build-hydraJobs: misc

--- build-hydraJobs.sh
+++ build-hydraJobs.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-PREFIX="$PWD#hydraJobs.x86_64-linux"
-nix eval --json "$PREFIX" |
-  jq --arg prefix "$PREFIX" -cr 'paths(strings) | join(".") | $prefix + "." + .' |
+nix-eval-jobs --flake .#hydraJobs.x86_64-linux --force-recurse --constituents |
+  jq -cr '.constituents + [.drvPath] | .[] + "^*"' |
   nom build --keep-going --no-link --print-out-paths --stdin
