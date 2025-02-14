4554bce0af4f66df638b3233251018699b32392a
Connor Baker <ConnorBaker01@gmail.com>
2025-02-13 16:00:38 -0800

    nccl: remove old comment

--- cuda-packages/common/nccl.nix
+++ cuda-packages/common/nccl.nix
@@ -23,13 +23,6 @@ stdenv.mkDerivation (finalAttrs: {
   pname = "nccl";
   version = "2.25.1-1";
 
-  # TODO: Build fails on CUDA 12.8 due to GCC 14 changes?
-  # cuda12.8-nccl> /nix/store/2s2ra7dqy7xs1qqd9qxzj7rvizdnhvc2-gcc-14-20241116/include/c++/14-20241116/type_traits(1610): error: "__is_nothrow_new_constructible" is not a function or static data member
-  # cuda12.8-nccl>       constexpr bool __is_nothrow_new_constructible
-  # cuda12.8-nccl>                      ^
-  # cuda12.8-nccl> /nix/store/2s2ra7dqy7xs1qqd9qxzj7rvizdnhvc2-gcc-14-20241116/include/c++/14-20241116/type_traits(1610): error: "constexpr" is not valid here
-  # cuda12.8-nccl>       constexpr bool __is_nothrow_new_constructible
-  # cuda12.8-nccl>       ^
   src = fetchFromGitHub {
     owner = "NVIDIA";
     repo = "nccl";
