9cd7a9d92e94d960682787dc15f62b7b1b230df4
Connor Baker <ConnorBaker01@gmail.com>
2025-05-02 11:37:19 -0700

    libcudss: fix builds on aarch64-linux

--- pkgs/development/cuda-modules/fixups/cudss/libcudss.nix
+++ pkgs/development/cuda-modules/fixups/cudss/libcudss.nix
@@ -9,9 +9,7 @@
 prevAttrs: {
   buildInputs =
     prevAttrs.buildInputs or [ ]
-    ++ [
-      libcublas
-    ]
+    ++ [ libcublas ]
     # MPI brings in NCCL dependency by way of UCC/UCX.
     ++ lib.optionals (!cudaStdenv.hasJetsonCudaCapability) [
       mpi
@@ -19,26 +17,7 @@ prevAttrs: {
     ];
 
   # Update the CMake configurations
-  # TODO: failing on cudaPackages_12_8 for SBSA:
-  # cuda12.8-libcudss> substituteStream() in derivation cuda12.8-libcudss-0.5.0.16: ERROR: pattern INTERFACE_LINK_DIRECTORIES\ \"/usr/local/cuda/targets/aarch64-linux/lib64\" doesn't match anything in file '/nix/store/spl124lj3aq1x1yzsjvahymc3kvw6z8d-cuda12.8-libcudss-0.5.0.16-dev/lib/cmake/cudss/cudss-static-targets.cmake'
   postFixup =
-    let
-      usrLocalCudaLib64Path = lib.concatStringsSep "/" (
-        [
-          "" # Leading slash
-          "usr"
-          "local"
-          "cuda"
-        ]
-        ++ lib.optionals stdenv.isAarch64 [
-          "targets"
-          "aarch64-linux"
-        ]
-        ++ [
-          "lib64"
-        ]
-      );
-    in
     prevAttrs.postFixup or ""
     + ''
       pushd "''${!outputDev:?}/lib/cmake/cudss" >/dev/null
@@ -53,10 +32,13 @@ prevAttrs: {
           "set(_cudss_search_prefix \"''${!outputDev:?}/lib;''${!outputLib:?}/lib;''${!outputInclude:?}/include\")"
 
       nixLog "patching $PWD/cudss-static-targets.cmake to fix INTERFACE_LINK_DIRECTORIES for cublas"
-      substituteInPlace "$PWD/cudss-static-targets.cmake" \
-        --replace-fail \
-          'INTERFACE_LINK_DIRECTORIES "${usrLocalCudaLib64Path}"' \
-          'INTERFACE_LINK_DIRECTORIES "${lib.getLib libcublas}/lib"'
+      sed -Ei \
+        's|INTERFACE_LINK_DIRECTORIES "/usr/local/cuda.*/lib64"|INTERFACE_LINK_DIRECTORIES "${lib.getLib libcublas}/lib"|g' \
+        "$PWD/cudss-static-targets.cmake"
+      if grep -Eq 'INTERFACE_LINK_DIRECTORIES "/usr/local/cuda.*/lib64"' "$PWD/cudss-static-targets.cmake"; then
+        nixErrorLog "failed to patch $PWD/cudss-static-targets.cmake"
+        exit 1
+      fi
 
       nixLog "patching $PWD/cudss-static-targets-release.cmake to fix the path to the static library"
       substituteInPlace "$PWD/cudss-static-targets-release.cmake" \
