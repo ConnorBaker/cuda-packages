9cd297f7196dc326a39ba3e6a80dae9b5c916ff4
Connor Baker <ConnorBaker01@gmail.com>
2025-05-06 02:32:36 +0000

    python3Packages.torch-tensorrt: 2.5.0 -> 2.6.0

--- pkgs/development/python-modules/torch-tensorrt/default.nix
+++ pkgs/development/python-modules/torch-tensorrt/default.nix
@@ -14,17 +14,20 @@
   torch,
   typing-extensions,
 }:
+let
+  version = "2.6.0";
+in
 buildPythonPackage {
   __structuredAttrs = true;
 
   pname = "torch-tensorrt";
-  version = "2.5.0";
+  inherit version;
 
   src = fetchFromGitHub {
     owner = "pytorch";
     repo = "TensorRT";
-    tag = "v2.5.0";
-    hash = "sha256-L5rk3EEPloNNAeYEjwX7cOeTId6XW/Y54mmwHf4vzeY=";
+    tag = "v${version}";
+    hash = "sha256-DAdorB4PD7UvWJNiZq79QLiWR51OnFvhCLXSBnNkxnc=";
   };
 
   disabled = pythonOlder "3.7";
@@ -34,7 +37,7 @@ buildPythonPackage {
   env = {
     RELEASE = "1";
     PYTHON_ONLY = "1"; # to avoid Bazel hellscape
-    BUILD_VERSION = "2.5.0";
+    BUILD_VERSION = version;
   };
 
   build-system = [
@@ -55,26 +58,32 @@ buildPythonPackage {
     nixLog "patching $PWD/dev_dep_versions.yml"
     substituteInPlace "$PWD/dev_dep_versions.yml" \
       --replace-fail \
-        '__cuda_version__: "12.4"' \
+        '__cuda_version__: "12.6"' \
         '__cuda_version__: "${cudaPackages.cudaMajorMinorVersion}"' \
       --replace-fail \
-        '__tensorrt_version__: "10.3.0"' \
+        '__tensorrt_version__: "10.7.0.post1"' \
         '__tensorrt_version__: "${cudaLib.utils.majorMinorPatch tensorrt.version}"'
 
     nixLog "patching $PWD/pyproject.toml"
     substituteInPlace "$PWD/pyproject.toml" \
       --replace-fail \
-        "tensorrt-cu12==10.3.0" \
+        "tensorrt-cu12>=10.7.0.post1,<10.8.0" \
         "tensorrt" \
       --replace-fail \
-        '"tensorrt-cu12-bindings==10.3.0",' \
-        "" \
-      --replace-fail \
-        '"tensorrt-cu12-libs==10.3.0",' \
-        "" \
+        "torch==2.6.0" \
+        "torch" \
       --replace-fail \
         "pybind11==2.6.2" \
-        "pybind11"
+        "pybind11" \
+      --replace-fail \
+        '"tensorrt>=10.7.0.post1,<10.8.0",' \
+        "" \
+      --replace-fail \
+        '"tensorrt-cu12-bindings>=10.7.0,<10.8.0",' \
+        "" \
+      --replace-fail \
+        '"tensorrt-cu12-libs>=10.7.0,<10.8.0",' \
+        ""
   '';
 
   dependencies = [
@@ -96,6 +105,9 @@ buildPythonPackage {
   meta = {
     description = "PyTorch/TorchScript/FX compiler for NVIDIA GPUs using TensorRT";
     homepage = "https://pytorch.org/TensorRT/";
+    # If the majorMinor version of the package is not the same as the majorMinor version of the
+    # torch version, then the package is broken.
+    broken = lib.versions.majorMinor torch.version != lib.versions.majorMinor version;
     license = lib.licenses.bsd3;
     platforms = [
       "aarch64-linux"
