7604ebdb8e9484c633710b408e50816e95ebfac9
Connor Baker <ConnorBaker01@gmail.com>
2025-11-25 14:35:43 +0000

    bump flake.lock; remove cudnn-frontend since newer version is now upstreamed
    Signed-off-by: Connor Baker <ConnorBaker01@gmail.com>

--- overlay.nix
+++ overlay.nix
@@ -207,11 +207,6 @@ let
             #     - nvccHook
             #
             #   These may not interact well with upstream's hooks and should be redesigned.
-
-            # Newer than upstream
-            cudnn-frontend =
-              finalCudaPackages.callPackage ./pkgs/development/cuda-modules/packages/cudnn-frontend.nix
-                { };
           })
         ];
       }
